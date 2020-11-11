59c767175e24d064d7c7006460cfece9e85605b1
rijkvp <rijkvanputten@hotmail.nl>
2020-11-11 14:14:58 +0100

    Huge progress

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
+
+var persons = [];
+var allResultsSection;
+var allResultsContainer;
+var allResultsConfirmation;
+var showingAllResults = false;
+var personalResultsSection;
+var personalResultsView;
+var personalResultsSelect;
+var personalResultsText;
+
 window.onload = (event) => {
     updatePersonForm();
+    
+    // Hide sections
+    allResultsSection = document.getElementById("allResultsSection");
+    allResultsSection.style.display = "none";
+    personalResultsSection = document.getElementById("personalResultsSection");
+    personalResultsSection.style.display = "none";
+
+    allResultsConfirmation = document.getElementById("allResultsConfirmation");
+    allResultsContainer = document.getElementById("allResultsContainer");
+    toggleAllResults(showingAllResults);
+
+    personalResultsSelect = document.getElementById("personalResultsSelect");
+    personalResultsView = document.getElementById("personalResultsView");
+    personalResultsView.style.display = "none";
+    personalResultsText = document.getElementById("personalResultsText");
 };
 
 class Person {
@@ -10,7 +36,6 @@ class Person {
 }
 
 function updatePersonForm() {
-    console.l
     var personList = document.getElementById("personList");
     personList.innerHTML = ""; // Clear previous
 
@@ -32,7 +57,7 @@ function shuffleArray(array) {
 }
 
 function execute() {
-    var persons = [];
+    persons.length = 0;
     var idCounter = 0;
     document.querySelectorAll('.personItem').forEach(
         personItemElement => {
@@ -53,18 +78,64 @@ function execute() {
         persons[i].selected = selected;
     }
 
-    persons.sort((a, b) => (a.id > b.id) ? 1 : -1)
-    
-    var resultsElement = document.getElementById("resultsDiv");
-    resultsElement.style.display = 'block';
+    persons.sort((a, b) => (a.id > b.id) ? 1 : -1);
 
-    var results = document.getElementById("results");
-    results.innerHTML = ""; // Clear resutls
+    updateResults();
+}
+
+function updateResults() {
+    toggleAllResults(false);
+
+    allResultsSection.style.display = 'block';
+    personalResultsSection.style.display = 'block';
+    personalResultsView.style.display = "none";
+
+    // Clear previous resutls
+    allResultsContainer.innerHTML = "";
+    personalResultsSelect.innerHTML = '<option selected value="default">Kies een persoon</option>'; // Default option
+    personalResultsText.innerHTML = "[NAAM HIER]";
 
     persons.forEach(person => {
         var resultElement = document.createElement("div");
         resultElement.className = "container my-3 p-4 text-left shadow rounded";
         resultElement.innerHTML = '<h5 class="display-6">' + person.name + '</h5><h5>' + person.selected + "</h5>";
-        results.appendChild(resultElement);
+        allResultsContainer.appendChild(resultElement);
+
+        var optionElement = document.createElement("option");
+        optionElement.setAttribute("value", person.id);
+        optionElement.innerHTML = person.name;
+        personalResultsSelect.appendChild(optionElement);
     });
+
+
+}
+
+function viewPersonalResult() {
+    var selectedIndex = personalResultsSelect.value;
+    if (selectedIndex != "default")
+    {
+        var selectedPerson = persons[selectedIndex];
+        personalResultsText.innerHTML = selectedPerson.selected;
+        personalResultsView.style.display = "block";
+    }
+}
+
+function hidePersonalResult() {
+    personalResultsText.innerHTML = "[NAAM HIER]";
+    personalResultsView.style.display = "none";
+}
+
+function toggleAllResults(value)
+{
+    showingAllResults = value;
+    if (showingAllResults)
+    {
+        allResultsConfirmation.style.display = "none";
+        allResultsContainer.style.display = "block";
+    } 
+    else
+    {
+        allResultsContainer.style.display = "none";
+        allResultsConfirmation.style.display = "block";
+    }
 }
